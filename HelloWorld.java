43be60832f7b0f1d65b7669e38f62d5a4a873861
ajinkya2006 <43513616+ajinkya2006@users.noreply.github.com>
2024-06-24 13:09:56 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld{
 	public static void main(String[] args){
 
 		System.out.println("Hello and welcome to Jenkins!");
+		System.out.println("No longer a helloworld. Changes are fetched from Git!");
+		
 	}
 }
